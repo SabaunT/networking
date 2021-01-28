2ca386cb2e4c83aa3a3cf5d0d46239a05da1fc3e
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-01-28 13:04:44 +0300

    cleaned connection handling

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -2,9 +2,6 @@
 //!
 //! todo Explain how it works
 
-// TODO
-// 1. ProxyStream logic to HttpProxy struct, which can perform server tasks (reading requests) and client tasks (sending requests and reading response)
-
 #[macro_use]
 extern crate anyhow;
 
@@ -16,10 +13,7 @@ use std::net::{TcpListener, TcpStream};
 use anyhow::Error;
 
 use cache::Cache;
-use http::{ProxyListener, ProxyStream};
-
-// Client request to proxy server meta - data
-const REQ_SIZE: usize = 4096;
+use http::{ProxyServer, ProxyClient};
 
 fn main() {
     loop {
@@ -31,7 +25,7 @@ fn main() {
 
 fn run() -> Result<(), Error> {
     let mut cache = Cache::default();
-    let proxy_server_socket = ProxyListener::bind("127.0.0.1:8080")?;
+    let proxy_server_socket = TcpListener::bind("127.0.0.1:8080")?;
 
     for stream in proxy_server_socket.incoming() {
         // todo multithread
@@ -42,24 +36,21 @@ fn run() -> Result<(), Error> {
 }
 
 fn handle_connection(mut stream: TcpStream, cache: Cache) -> Result<(), Error> {
-    let mut client_buf = [0; REQ_SIZE];
-    let _ = stream.read(&mut client_buf[..])?;
+    let mut proxy_server = ProxyServer::from_stream(stream);
+    let req = proxy_server.read_req()?;
 
-    let client_req = http::Request::try_from(client_buf.as_ref())?;
     {
         let mut c = cache.lock().expect("todo msg");
-        let target_url = client_req.target_url();
+        let target_url = req.target_url();
 
         if let Some(data) = c.get(&target_url) {
-            stream.write(data)?;
-            stream.flush()?;
+            proxy_server.send_resp(data)?;
         } else {
-            let mut s = ProxyStream::connect(client_req.authority())?;
-            s.send_req(&client_req)?;
-            let caching_buf = s.read_resp()?;
+            let mut proxy_client = ProxyClient::connect(req.authority())?;
+            proxy_client.send_req(&req.serialize())?;
+            let caching_buf = proxy_client.read_resp()?;
 
-            stream.write(&caching_buf)?;
-            stream.flush()?;
+            proxy_server.send_resp(&caching_buf)?;
 
             c.insert(target_url, caching_buf);
         }
@@ -77,70 +68,10 @@ mod http {
     use httparse;
     use url::Url;
 
-    pub(super) struct ProxyStream(TcpStream);
+    const REQ_END: &[u8; 4] = b"\r\n\r\n";
 
-    impl ProxyStream {
-        pub(super) fn connect<A: ToSocketAddrs>(addr: A) -> Result<Self, Error> {
-            TcpStream::connect(addr).map(ProxyStream).map_err(|e| e.into())
-        }
-
-        pub(super) fn send_req(&mut self, req: &Request) -> Result<(), Error> {
-            let ProxyStream(ref mut stream) = self;
-            let ser_req = req.serialize(); // todo use builder
-            stream.write(&ser_req)?;
-            stream.flush()?;
-            Ok(())
-        }
-
-        pub(super) fn read_resp(&mut self) -> Result<Vec<u8>, Error> {
-            let ProxyStream(ref mut stream) = self;
-            let mut ret = Vec::new();
-            loop {
-                let mut buf = [0; 512];
-                match stream.read(&mut buf) {
-                    Ok(0) => break,
-                    Ok(i) => {
-                        ret.extend_from_slice(&buf[..i]);
-                    }
-                    Err(e) => return Err(anyhow!(e)),
-                }
-            }
-            Ok(ret)
-        }
-    }
-
-    impl Deref for ProxyStream {
-        type Target = TcpStream;
-
-        fn deref(&self) -> &Self::Target {
-            &self.0
-        }
-    }
-
-    pub(super) struct ProxyListener(TcpListener);
-
-    impl ProxyListener {
-        pub(super) fn bind<A: ToSocketAddrs>(addr: A) -> Result<Self, Error> {
-            TcpListener::bind(addr).map(ProxyListener).map_err(|e| e.into())
-        }
-    }
-
-    impl Deref for ProxyListener {
-        type Target = TcpListener;
-
-        fn deref(&self) -> &Self::Target {
-            &self.0
-        }
-    }
-
-    #[derive(Default)]
-    pub(super) struct RequestBuilder {
-        method: Option<String>,
-        host: Option<String>,
-        path: Option<String>,
-        port: Option<u16>,
-        // headers
-    }
+    pub(super) struct ProxyServer(TcpStream);
+    pub(super) struct ProxyClient(TcpStream);
 
     pub(super) struct Request {
         pub(super) method: String,
@@ -150,44 +81,63 @@ mod http {
         // headers
     }
 
-    // impl RequestBuilder {
-    //     pub(super) fn new() -> Self {
-    //         Self::default()
-    //     }
-    //
-    //     pub(super) fn build(self) -> Result<Request, Error> {
-    //         let RequestBuilder { method, host, path, port } = self;
-    //         if let (Some(method), Some(host), Some(path), Some(port)) = (method, host, path, port) {
-    //             return Ok( Request { method, host, path, port} )
-    //         }
-    //         return Err(anyhow!("Can't build request: not all fields are set enough data"));
-    //     }
-    //
-    //     pub(super) fn method(self, m: String) -> Self {
-    //         let RequestBuilder { method: _, host, path, port } = self;
-    //         Self { method: Some(m), host, path, port }
-    //     }
-    //
-    //     pub(super) fn host(self, h: String) -> Self {
-    //         let RequestBuilder { method, host: _, path, port } = self;
-    //         Self { method, host: Some(h), path, port }
-    //     }
-    //
-    //     pub(super) fn path(self, p: String) -> Self {
-    //         let RequestBuilder { method, host, path: _, port } = self;
-    //         Self { method, host, path: Some(p), port }
-    //     }
-    //
-    //     pub(super) fn port(self, p: u16) -> Self {
-    //         let RequestBuilder { method, host, path, port: _ } = self;
-    //         Self { method, host, path, port: Some(p) }
-    //     }
-    // }
+    impl ProxyServer {
+        pub(super) fn from_stream(stream: TcpStream) -> Self {
+            Self(stream)
+        }
+
+        pub(super) fn send_resp(&mut self, resp: &[u8]) -> Result<(), Error> {
+            let ProxyServer(ref mut stream) = self;
+            stream.write(&resp)?;
+            stream.flush().map(|_| ()).map_err(|e| e.into())
+        }
+
+        pub(super) fn read_req(&mut self) -> Result<Request, Error> {
+            let ProxyServer(ref mut stream) = self;
+            let mut req_bytes = Vec::new();
+            loop {
+                let mut buf = [0; 512];
+                match stream.read(&mut buf) {
+                    Ok(i) => {
+                        req_bytes.extend_from_slice(&buf[..i]);
+                        if buf[..i].ends_with(REQ_END) { break; }
+                    }
+                    Err(e) => return Err(anyhow!(e)),
+                }
+            }
+            Request::try_from(req_bytes.as_slice())
+        }
+    }
+
+    impl ProxyClient {
+        pub(super) fn connect<A: ToSocketAddrs>(addr: A) -> Result<Self, Error> {
+            TcpStream::connect(addr).map(ProxyClient).map_err(|e| e.into())
+        }
+
+        pub(super) fn send_req(&mut self, req: &[u8]) -> Result<(), Error> {
+            let ProxyClient(ref mut stream) = self;
+            stream.write(req)?;
+            stream.flush().map(|_| ()).map_err(|e| e.into())
+        }
+
+        pub(super) fn read_resp(&mut self) -> Result<Vec<u8>, Error> {
+            let ProxyClient(ref mut stream) = self;
+            let mut resp_bytes = Vec::new();
+            loop {
+                let mut buf = [0; 512];
+                match stream.read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(i) => {
+                        resp_bytes.extend_from_slice(&buf[..i]);
+                    }
+                    Err(e) => return Err(anyhow!(e)),
+                }
+            }
+            Ok(resp_bytes)
+        }
+    }
 
     impl Request {
-        // pub(super) fn builder() -> RequestBuilder {
-        //     RequestBuilder::new()
-        // }
 
         pub(super) fn target_url(&self) -> String {
             format!("{}:{}{}", self.host, self.port, self.path)
@@ -200,7 +150,7 @@ mod http {
         pub(super) fn serialize(&self) -> Vec<u8> {
             // Closing connection due to https://tools.ietf.org/html/rfc2616#section-8.1.2
             let req = format!(
-                "{} {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: MyProxy/7.58.0\r\nAccept: */*\r\nConnection: close\r\n\r\n",
+                "{} {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: SabaunTProxy/0.0.1\r\nAccept: */*\r\nConnection: close\r\n\r\n",
                 self.method, self.path, self.host
             );
             req.as_bytes().into()
@@ -218,9 +168,9 @@ mod http {
             r.parse(data)?;
 
             let method = r.method.map(ToString::to_string).ok_or(anyhow!("Invalid request: no http method"))?;
-            if let Some(p) = r.path {
-                let p = p.trim_start_matches("/");
-                let url = Url::parse(p)?;
+            if let Some(path) = r.path {
+                let path = path.trim_start_matches("/");
+                let url = Url::parse(path)?;
                 let host = url.host_str().map(ToString::to_string).ok_or(anyhow!("Invalid request: no resource url"))?;
                 let path = url.path().to_string();
                 let port = url.port_or_known_default().ok_or(anyhow!("Invalid request: url scheme default port"))?;
